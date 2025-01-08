8f9e908a41df5c21beb83e2ce870febb2adf788f
Troy Garner <troy.garner@tbdenterprises.ca>
2025-01-08 12:14:04 -0700

    fix: Updated app identifier in configuration.

--- Program/MyApplication/Configuration/HostConfig.cs
+++ Program/MyApplication/Configuration/HostConfig.cs
@@ -4,12 +4,6 @@ namespace MyApplication.Configuration;
 
 public sealed class HostConfig
 {
-    private readonly string _configurationPath = null!;
     public string Environment { get; init; } = null!;
-
-    public string ConfigurationPath
-    {
-        get => _configurationPath.Replace("{APP_IDENTIFIER}", ApplicationEnvironment.AppIdentifier, StringComparison.Ordinal);
-        init => _configurationPath = value;
-    }
+    public string ConfigurationPath { get; init; } = null!;
 }
